f894e36dfb992dbcdbbc9b3b631ace59857497ce
Florian <45694132+flo-bit@users.noreply.github.com>
2024-06-21 23:32:32 +0200

    update projects, add depth 3d avatar

---
+++ src/lib/3D/Depth3D/Depth3D.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import Scene3D from './Scene3D.svelte';
+
+	export let image: {
+		image: string;
+		depth: string;
+	};
+</script>
+
+<Canvas>
+	<Scene3D {image} {...$$restProps} />
+</Canvas>
---
+++ src/lib/3D/Depth3D/index.ts
@@ -0,0 +1,2 @@
+export { default as Depth3D } from './Depth3D.svelte';
+export { default } from './Depth3D.svelte';
